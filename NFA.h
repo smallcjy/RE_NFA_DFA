94feb4404229216d204945a857eb08f6988cefb7
smallc <2628035541@qq.com>
2024-03-29 18:54:21 +0800

    实现RE—NFA的接口

--- NFA.h
+++ NFA.h
@@ -2,14 +2,38 @@
 #include<string>
 using namespace std;
 
-//State class
-class State{
-    private:
-        
-}
+//Transfer struct
+struct Transfer
+{
+    int node_from;
+    int node_out;
+    char symbol;
+};
+
 
 //NFA class
 class NFA{
     private:
+        //非终态
+        vector<int> states;
+        vector<Transfer> Tranfers;
+        int final_state;
+    public:
+        NFA();
+        int get_num_states();
+        void set_state(int);
+        void set_transfer(int,int,char);
+        void set_final_state(int);
+        int get_final_state();
+        void display();
+};
 
-}
+//NFA's construct func
+//连接NFA
+NFA concat(NFA,NFA);
+//或NFA
+NFA or_op(vector<NFA> select_NFAs, int no_of_selections);
+//闭包NFA
+NFA kleene(NFA a);
+//RE转NFA
+NFA re_to_nfa(void);    //这里用你的树作为输入
